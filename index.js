83bb65890f34a365022f28889685242b36d853cf
RebeccaEvans <evans.rebeccaj@gmail.com>
2019-12-15 13:37:09 -0800

    dinosaur crud completed files

--- index.js
+++ index.js
@@ -1,32 +1,68 @@
 let express = require('express')
 let layouts = require('express-ejs-layouts')
 let  fs = require('fs');
-
-
 let app = express()
 
-// Set up and middleware
+//middleware
 app.set('view engine', 'ejs')
 app.use(layouts)
 app.use('/', express.static('static'))
+app.use(express.urlencoded({extended: false}));
+
+//routes
+// app.get('/dinosaurs', function(req, res) {
+// 	var dinosaurs = fs.readFileSync('./dinosaurs.json');
+// 	var dinoData = JSON.parse(dinosaurs);
+// 	res.render('dinosaurs/index', {myDinos: dinoData});
+//   });
 
 app.get('/dinosaurs', function(req, res) {
 	var dinosaurs = fs.readFileSync('./dinosaurs.json');
 	var dinoData = JSON.parse(dinosaurs);
+	console.log(dinoData)
+	var nameFilter = req.query.nameFilter;
+	console.log(nameFilter)
+
+	if (nameFilter) {
+		dinoData = dinoData.filter(function(dino) {
+		return dino.name.toLowerCase() === nameFilter.toLowerCase();
+		});
+	}
 	res.render('dinosaurs/index', {myDinos: dinoData});
+});
+
+
+app.get('/dinosaurs/new', function(req, res){
+	res.render('dinosaurs/new');
+});
+
+
+  app.post('/dinosaurs', function(req, res) {
+	// read dinosaurs file
+	var dinosaurs = fs.readFileSync('./dinosaurs.json');
+	dinosaurs = JSON.parse(dinosaurs);
+	// add item to dinosaurs array
+	dinosaurs.push(req.body);
+	// save dinosaurs to the data.json file
+	fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinosaurs));
+  
+	//redirect to the GET /dinosaurs route (index)
+	res.redirect('/dinosaurs');
   });
 
+
 app.get('/dinosaurs/:idx', function(req, res) {
-var dinosaurs = fs.readFileSync('./dinosaurs.json');
-var dinoData = JSON.parse(dinosaurs);
-var dinoIndex = parseInt(req.params.idx);
-	if (dinoIndex<dinoData.length) {
-		res.render('dinosaurs/show', {myDino: dinoData[dinoIndex]})
-	}
-	else {
+	var dinosaurs = fs.readFileSync('./dinosaurs.json');
+	var dinoData = JSON.parse(dinosaurs);
+	var dinoIndex = parseInt(req.params.idx);
+  
+	res.render('dinosaurs/index', {myDinos: dinoData});
+	if (dinoIndex>(dinoData.length-1) && typeof(dinoIndex) === 'number') {
 		res.render('error')
 	}
-res.render('dinosaurs/show', {myDino: dinoData[dinoIndex]});
+	else {
+		res.render('dinosaurs/show', {myDino: dinoData[dinoIndex]})
+	}
 });
 
 
