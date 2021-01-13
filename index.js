7efb6af99ae7abfda08de653bac7c198d2f10d6d
Your Name <you@example.com>
2021-01-12 23:39:42 -0800

    Added ability to add new prehistoric creature, have the form redirect to the list of creatures, and have the new creature show up on that list.

--- index.js
+++ index.js
@@ -72,7 +72,6 @@ app.get('/prehistoric_creatures', (req, res) => {
 })
 
 app.get('/prehistoric_creatures/new', (req, res) => {
-
     res.render('./prehistoric_creatures/new.ejs')
 })
 
@@ -84,6 +83,11 @@ app.get('/prehistoric_creatures/:idx', (req, res) => {
 })
 
 app.post('/prehistoric_creatures', (req, res) => {
+    let creatures = fs.readFileSync('./prehistoric_creatures.json')
+    let creatureData = JSON.parse(creatures) // Convert from JSON to JavaScript.
+    creatureData.push(req.body)
+    fs.writeFileSync('./prehistoric_creatures.json', JSON.stringify(creatureData)) // Convert back to JSON.
+    console.log(req.body)
     res.redirect('/prehistoric_creatures')
 })
 
